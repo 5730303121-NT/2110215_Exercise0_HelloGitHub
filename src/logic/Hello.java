53b472ac57412681cb3de159dbf4178d6aab4f0c
5730303121NT <baank06092538>
2015-08-21 11:28:10 +0700

    i love u

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 
 	public static String greetingMessage(){
 		// TODO Implement this method to return string "Hello GitHub".
-		return "";
+		return "i love u";
 	}
 	
 	public static int[] bubbleSort(int[] array){
